51d07cf47aaf58f5737df39efc318e0a36f72d52
Saif <145370684+itg6j@users.noreply.github.com>
2026-08-16 10:53:27 -0700

    Remove redundant code in UDPheader.py

--- UDPheader.py
+++ UDPheader.py
@@ -1,6 +1,5 @@
 import socket
 import struct
-
 # 8byte header hex
 header = "CB84000D001C001C"
 src_ip = "192.168.1.1"
@@ -17,15 +16,12 @@ data = total1 - 8
 def calculate_checksum(data_bytes):
     if len(data_bytes) % 2 != 0:
         data_bytes += b"\x00"
-
     total_sum = 0
     for i in range(0, len(data_bytes), 2):
         word = (data_bytes[i] << 8) + data_bytes[i + 1]
         total_sum += word
-
     while total_sum >> 16:
         total_sum = (total_sum & 0xFFFF) + (total_sum >> 16)
-
     return ~total_sum & 0xFFFF
 src_ip_bytes = bytes([int(x) for x in src_ip.split(".")])
 dest_ip_bytes = bytes([int(x) for x in dest_ip.split(".")])
